363516f33ae11801dd8c8f173c60b4bf8e579dec
sdp <sdp@spr-bkc-pc.jf.intel.com>
2022-04-29 17:00:34 -0400

    Add write to file

--- hello.py
+++ hello.py
@@ -8,6 +8,8 @@ def main():
     try:        
         while True:
             print(VALUE)
+            with open("log.txt", "a") as myfile:
+                myfile.write(VALUE)
             time.sleep(5)
     except KeyboardInterrupt:
         sys.exit(1)
